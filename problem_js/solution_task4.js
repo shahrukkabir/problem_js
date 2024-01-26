207b5eb6126aa4473b7a6237912a80e94960e27e
Shahruk Kabir <shahrukkabir01@gmail.com>
2024-01-26 15:32:11 +0600

    final commit

--- problem_js/solution_task4.js
+++ problem_js/solution_task4.js
@@ -1,4 +1,4 @@
-var a = isNaN(‘11’); //false because the string'11' can be converted to a valid number
+var a = isNaN('11'); //false because the string'11' can be converted to a valid number
 var a = isNaN(2-10); // false because the expression 2-10=8,which is a valid number
 //In both case,'isNaN'checks if the provided value is Not a Number
 
