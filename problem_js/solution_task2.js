f0037b0f8ae4837eb117a0d0650cc87d0c02a4ac
Shahruk Kabir <shahrukkabir01@gmail.com>
2024-01-26 15:31:03 +0600

    solve four problems

---
+++ problem_js/solution_task2.js
@@ -0,0 +1,8 @@
+var math=75.25;
+var bio=65;
+var chem=80;
+var phy=35.45;
+var ban=99.50;
+var sum=math+bio+chem+phy+ban;
+var av =(sum/5);
+console.log(av.toFixed(2));
---
+++ problem_js/solution_task3.js
@@ -0,0 +1,4 @@
+var number1 = 119;
+var number2 = 5;
+var rem = number1 % number2;
+console.log(rem);
